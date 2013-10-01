ca6840c479aa3cdcd00291bfe1970db5a0235f98
Marius Mandal <mariusmandal@gmail.com>
2013-10-01 11:02:37 +0200

    wp-weakness fix

--- UKMresources.php
+++ UKMresources.php
@@ -20,6 +20,6 @@ function UKMresources() {
 	wp_enqueue_script('jqueryGoogleUI', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js');
 	wp_enqueue_script('wp-admin_smslink', plugin_dir_url( __FILE__ ) .'js/wp-admin.smslink.js');
 	
-	wp_enqueue_script('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'js/UKMresources_wp-admin.js');
+	wp_enqueue_script('UKMresources_wp-adminJS', plugin_dir_url( __FILE__ ) .'js/UKMresources_wp-admin.js');
 
 }
