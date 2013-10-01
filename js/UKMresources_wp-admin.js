8e62d0f9c8b9df4bcf60331b9882143556a32354
Marius Mandal <mariusmandal@gmail.com>
2013-10-01 11:07:01 +0200

    init js on ready

--- js/UKMresources_wp-admin.js
+++ js/UKMresources_wp-admin.js
@@ -1,4 +1,6 @@
-jQuery('#hugesubmit').click(function(){
-	jQuery(this).find('#lagre').html('Lagrer...');
-	jQuery(this).parents('form').submit();
+jQuery(document).ready(function(){
+	jQuery('#hugesubmit').click(function(){
+		jQuery(this).find('#lagre').html('Lagrer...');
+		jQuery(this).parents('form').submit();
+	});
 });
