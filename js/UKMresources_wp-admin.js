cd6448e9e354665f921753492a148372414766cb
Marius Mandal <mariusmandal@gmail.com>
2013-10-01 11:11:14 +0200

    fancy fix to submit form

--- js/UKMresources_wp-admin.js
+++ js/UKMresources_wp-admin.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
 	jQuery('#hugesubmit').click(function(){
 		jQuery(this).find('#lagre').html('Lagrer...');
-		jQuery(this).parents('form').submit();
+		jQuery(this).parents('form').find('input[type=submit]').click();
 	});
 });
