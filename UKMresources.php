b68b32619595539d365475ea089ef68bf5565bb7
Marius Mandal <mariusmandal@gmail.com>
2013-10-01 11:01:22 +0200

    fix paths

--- UKMresources.php
+++ UKMresources.php
@@ -13,13 +13,13 @@ add_action( 'admin_enqueue_scripts', 'UKMresources' );
 
 
 function UKMresources() {
-	wp_enqueue_style( 'jquery-ui-style', plugin_dir_url( __FILE__ ) .'/css/jquery-ui-1.7.3.custom.css');
-	wp_enqueue_style('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'/js/UKMresources_wp-admin.css');
+	wp_enqueue_style( 'jquery-ui-style', plugin_dir_url( __FILE__ ) .'css/jquery-ui-1.7.3.custom.css');
+	wp_enqueue_style('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'js/UKMresources_wp-admin.css');
 	
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('jqueryGoogleUI', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js');
-	wp_enqueue_script('wp-admin_smslink', plugin_dir_url( __FILE__ ) .'/js/wp-admin.smslink.js');
+	wp_enqueue_script('wp-admin_smslink', plugin_dir_url( __FILE__ ) .'js/wp-admin.smslink.js');
 	
-	wp_enqueue_script('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'/js/UKMresources_wp-admin.js');
+	wp_enqueue_script('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'js/UKMresources_wp-admin.js');
 
 }
