6e8522a40bfb8ca53ec63698d453e66f746a4d59
Marius Mandal <mariusmandal@gmail.com>
2013-10-09 12:50:14 +0200

    ukmsms function?

--- js/wp-admin.smslink.js
+++ js/wp-admin.smslink.js
@@ -1,10 +1,23 @@
 jQuery(document).ready(function(){
+	var url = window.location.href.split('?')[0] + '?page=UKMSMS_gui';
+	var form = jQuery('<form action="' + url + '" method="post" id="UKMSMS_form">' +
+						 '<input type="hidden" name="sms_to" id="UKMSMS_to" value="" />' +
+					  '</form>');
+	jQuery('body').append(form);
+
+
+
 	registerUKMSMS('body');
+	
+	jQuery.on('click', '.UKMSMS_link', function() {
+		jQuery('#UKMSMS_to').val(jQuery(this).attr('data-to'));
+		jQuery('#UKMSMS_form').submit();
+	});
 });
 
 
 function registerUKMSMS(selector) {
 	jQuery(selector).find('.UKMSMS').each(function(){
-		jQuery(this).html( '<a href="javascript:alert(\'kommer snart \')">'+ jQuery(this).html() + '</a>');
+		jQuery(this).html( '<a href="#" class="UKMSMS_link" data-to="'+jQuery(this).html()+'">'+ jQuery(this).html() + '</a>');
 	});
 }
