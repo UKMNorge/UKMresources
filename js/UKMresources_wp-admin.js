5ba8644d0191ccacff4e9d3efed5b3252f1848d6
Marius Mandal <mariusmandal@gmail.com>
2013-10-25 13:07:19 +0200

    Add URL Param to jQuery

--- js/UKMresources_wp-admin.js
+++ js/UKMresources_wp-admin.js
@@ -4,3 +4,10 @@ jQuery(document).ready(function(){
 		jQuery(this).parents('form').find('input[type=submit]').click();
 	});
 });
+
+
+
+jQuery.urlParam = function(name){
+    var results = new RegExp('[\\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+    return results[1] || 0;
+}
