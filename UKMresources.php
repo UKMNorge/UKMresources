6840f77a533a31bd4d9ad80a04c7d55a9c9d74b9
Marius Mandal <mariusmandal@gmail.com>
2013-11-11 11:25:18 +0100

    Additional bootstrap hooks
    To match hooks in wpsuccess
    https://github.com/mariusmandal/WPsuccess/

--- UKMresources.php
+++ UKMresources.php
@@ -28,5 +28,7 @@ function UKMresources() {
 
 	wp_register_script('bootstrap_js', plugin_dir_url( __FILE__ ) .'js/bootstrap.min.js');
 	wp_register_style('bootstrap_css', plugin_dir_url( __FILE__ ) .'css/bootstrap.min.css');
+	wp_register_script('WPbootstrap_js', plugin_dir_url( __FILE__ ) .'js/bootstrap.min.js');
+	wp_register_style('WPbootstrap_css', plugin_dir_url( __FILE__ ) .'css/bootstrap.min.css');
 
 }
