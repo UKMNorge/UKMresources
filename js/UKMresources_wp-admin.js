66603c18308f8a01ab273dc1a986023f77753a5f
AsgeirSH <askhus@online.no>
2016-10-05 13:40:12 +0200

    Flytt datepicker-translation til resources.

--- js/UKMresources_wp-admin.js
+++ js/UKMresources_wp-admin.js
@@ -17,6 +17,28 @@ jQuery(document).on('click','.toggle', function(e){
     }
 });
 
+jQuery(document).ready(function(){
+	jQuery.datepicker.regional['no'] = {
+		closeText: 'Lukk',
+        	prevText: '&laquo;Forrige',
+		nextText: 'Neste&raquo;',
+		currentText: 'I dag',
+	        monthNames: ['Januar','Februar','Mars','April','Mai','Juni',
+        		'Juli','August','September','Oktober','November','Desember'],
+	        monthNamesShort: ['Jan','Feb','Mar','Apr','Mai','Jun',
+        		'Jul','Aug','Sep','Okt','Nov','Des'],
+		dayNamesShort: ['S&oslash;n','Man','Tir','Ons','Tor','Fre','L&oslash;r'],
+		dayNames: ['S&oslash;ndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','L&oslash;rdag'],
+		dayNamesMin: ['S&oslash;','Ma','Ti','On','To','Fr','L&oslash;'],
+		weekHeader: 'Uke',
+       		dateFormat: 'yy-mm-dd',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
+    jQuery.datepicker.setDefaults(jQuery.datepicker.regional['no']);
+});
+
 jQuery.extend({
   getUrlVars: function(){
     var vars = [], hash;
