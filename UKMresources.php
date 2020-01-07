5d47d8415c15364a2207bc1fe8847e408e8d1c1a
Jim Frode Hoff <jimfrodehoff@gmail.com>
2020-01-07 15:11:24 +0100

    Expose UKM_HOSTNAME to frontend

--- UKMresources.php
+++ UKMresources.php
@@ -48,6 +48,7 @@ function UKMresources() {
 function UKMconstants() {
     echo '<script type="text/javascript">'.
             'var SEASON = '. get_site_option('season') .';'.
-            'var GOOGLE_API_KEY = "'. (defined('GOOGLE_API_KEY') ? GOOGLE_API_KEY : '') .'";' .
+			'var GOOGLE_API_KEY = "'. (defined('GOOGLE_API_KEY') ? GOOGLE_API_KEY : '') .'";' .
+			'var UKM_HOSTNAME = "' . UKM_HOSTNAME . '";' . 
         '</script>';
 }
