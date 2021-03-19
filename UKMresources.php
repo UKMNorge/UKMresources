cf3f51416a063b944054ec88d69ddcfa4874ca6e
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2021-03-19 12:58:42 +0100

    Endrer path til å peke statisk.
    Bruker en final variable for å definere statisk path-en for å peke på resurser.
    
    Deriverer fra: https://github.com/UKMNorge/UKMresources/issues/3
    
    OBS: UKMconfig.inc.php er modifisert. Denne linjen er lagt til:
    "# RESOURCES PLUGIN PATH
    define('PLUGIN_PATH', 'https://' . UKM_HOSTNAME . '/wp-content/plugins/');"

--- UKMresources.php
+++ UKMresources.php
@@ -12,39 +12,39 @@ Author URI: http://www.ukm-norge.no
 add_action( 'admin_enqueue_scripts', 'UKMresources', -1000 );
 add_action( 'admin_print_scripts', 'UKMconstants' );
 function UKMresources() {
-	wp_enqueue_style( 'jquery-ui-style', plugin_dir_url( __FILE__ ) .'css/jquery-ui-1.7.3.custom.css');
-	wp_enqueue_style('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'css/UKMresources_wp-admin.css');
-	wp_register_style('UKMresources_tabs', plugin_dir_url( __FILE__ ) .'css/UKMresources_tabs.css');
+	wp_enqueue_style( 'jquery-ui-style', PLUGIN_PATH . 'UKMresources/css/jquery-ui-1.7.3.custom.css');
+	wp_enqueue_style('UKMresources_wp-admin', PLUGIN_PATH . 'UKMresources/css/UKMresources_wp-admin.css');
+	wp_register_style('UKMresources_tabs', PLUGIN_PATH . 'UKMresources/css/UKMresources_tabs.css');
 	wp_enqueue_style('UKMresources_tabs');
 		
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('jqueryGoogleUI', '//code.jquery.com/ui/1.11.4/jquery-ui.min.js');
-	wp_enqueue_script('wp-admin_smslink', plugin_dir_url( __FILE__ ) .'js/wp-admin.smslink.js');
+	wp_enqueue_script('wp-admin_smslink', PLUGIN_PATH . 'UKMresources/js/wp-admin.smslink.js');
 	
-	wp_enqueue_script('UKMresources_wp-admin', plugin_dir_url( __FILE__ ) .'js/UKMresources_wp-admin.js?v2020-09-28');
-	wp_enqueue_script('UKMresources', plugin_dir_url( __FILE__ ) .'js/UKMresources.js');
+	wp_enqueue_script('UKMresources_wp-admin', PLUGIN_PATH . 'UKMresources/js/UKMresources_wp-admin.js?v2020-09-28');
+	wp_enqueue_script('UKMresources', PLUGIN_PATH . 'UKMresources/js/UKMresources.js');
 
-	wp_register_script('handlebars_js', plugin_dir_url( __FILE__ ) .'js/handlebars.js');
+	wp_register_script('handlebars_js', PLUGIN_PATH . 'UKMresources/js/handlebars.js');
 
-	wp_register_script('jQuery-fastlivefilter', plugin_dir_url( __FILE__ ) .'js/jquery.fastLiveFilter.js');
-    wp_register_script('jQuery_autogrow', plugin_dir_url( __FILE__ ) .'js/autogrow.jquery.js');
+	wp_register_script('jQuery-fastlivefilter', PLUGIN_PATH . 'UKMresources/js/jquery.fastLiveFilter.js');
+    wp_register_script('jQuery_autogrow', PLUGIN_PATH . 'UKMresources/js/autogrow.jquery.js');
 
-	wp_register_script('bootstrap_js', plugin_dir_url( __FILE__ ) .'js/bootstrap.min.js');
-	wp_register_style('bootstrap_css', plugin_dir_url( __FILE__ ) .'css/bootstrap.min.css');
-	wp_register_script('WPbootstrap_js', plugin_dir_url( __FILE__ ) .'js/bootstrap.min.js');
-	wp_register_style('WPbootstrap_css', plugin_dir_url( __FILE__ ) .'css/bootstrap.min.css');
+	wp_register_script('bootstrap_js', PLUGIN_PATH . 'UKMresources/js/bootstrap.min.js');
+	wp_register_style('bootstrap_css', PLUGIN_PATH . 'UKMresources/css/bootstrap.min.css');
+	wp_register_script('WPbootstrap_js', PLUGIN_PATH . 'UKMresources/js/bootstrap.min.js');
+	wp_register_style('WPbootstrap_css', PLUGIN_PATH . 'UKMresources/css/bootstrap.min.css');
 
-	wp_register_script('WPbootstrap3_js', plugin_dir_url( __FILE__ ) .'js/bootstrap3.js');
-	wp_register_style('WPbootstrap3_css', plugin_dir_url( __FILE__ ) .'css/bootstrap3.css');
-	wp_register_style('WPbootstrap3_outlinebtn', plugin_dir_url( __FILE__ ) .'css/bootstrap3.btn-outline.css');
+	wp_register_script('WPbootstrap3_js', PLUGIN_PATH . 'UKMresources/js/bootstrap3.js');
+	wp_register_style('WPbootstrap3_css', PLUGIN_PATH . 'UKMresources/css/bootstrap3.css');
+	wp_register_style('WPbootstrap3_outlinebtn', PLUGIN_PATH . 'UKMresources/css/bootstrap3.btn-outline.css');
 	
-	wp_register_script('WPbootstrap4_js', plugin_dir_url( __FILE__ ) .'js/bootstrap4-alpha2.min.js');
-	wp_register_style('WPbootstrap4_css', plugin_dir_url( __FILE__ ) .'css/bootstrap4-alpha2.min.css');
+	wp_register_script('WPbootstrap4_js', PLUGIN_PATH . 'UKMresources/js/bootstrap4-alpha2.min.js');
+	wp_register_style('WPbootstrap4_css', PLUGIN_PATH . 'UKMresources/css/bootstrap4-alpha2.min.css');
     
-    wp_enqueue_style('WPgallery_css', plugin_dir_url( __FILE__ ) .'css/gallery.css');
+    wp_enqueue_style('WPgallery_css', PLUGIN_PATH . 'UKMresources/css/gallery.css');
 	
-    wp_register_script( 'TwigJS', plugin_dir_url( __FILE__ ) .'js/twig.js');
-    wp_register_script( 'dropzone', plugin_dir_url( __FILE__ ) .'js/dropzone.js');
+    wp_register_script( 'TwigJS', PLUGIN_PATH . 'UKMresources/js/twig.js');
+    wp_register_script( 'dropzone', PLUGIN_PATH . 'UKMresources/js/dropzone.js');
 }
 
 function UKMconstants() {
